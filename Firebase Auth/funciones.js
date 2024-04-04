ebc5cf4fb8f2a07f6d90b6c49ed948e3b65a58fa
50DayEmpire <79984327+50DayEmpire@users.noreply.github.com>
2024-04-03 19:59:19 -0600

    aplicacion con firebase auth agregada

---
+++ Firebase Auth/funciones.js
@@ -0,0 +1,72 @@
+function ocultar(n){
+    elemento = document.getElementById(n)
+    elemento.style.display = "none"
+}
+function mostrar(n){
+    elemento = document.getElementById(n)
+    elemento.style.display = "block"
+}
+
+function crearCookie(){
+    ocultar('form');
+    auto()
+    mostrar('tarjetas')
+    const d = new Date();
+    d.setTime(d.getTime()+(10*1000));
+    let expira = d.toUTCString()
+    usuario = document.getElementById("usuario1").value
+    contra = document.getElementById("contra1").value
+    document.cookie = "usuario="+usuario+";expires="+expira+";path=/";
+    document.cookie = "contra="+contra+";expires="+expira+";path=/";
+}
+
+window.onload = function iniciar(){
+    const login = document.querySelector("#login");
+    login.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const user = document.querySelector("#usuario1").value;
+    const contra = document.querySelector("#contra1").value;
+
+    const credential = signInWithEmailAndPassword(auth,user,contra);
+    console.log(credential)
+    })
+}
+
+function auto(){
+    altura = document.getElementById("alt");
+    peso = document.getElementById("peso");
+    tiempo = document.getElementById("tiempo");
+
+    altura.value = sessionStorage.getItem("altura")
+    peso.value = sessionStorage.getItem("peso")
+    tiempo.value = sessionStorage.getItem("tiempo")
+
+    altura2 = document.getElementById("alt2");
+    peso2 = document.getElementById("peso2");
+    tiempo2 = document.getElementById("tiempo2");
+
+    altura2.value = localStorage.getItem("altura")
+    peso2.value = localStorage.getItem("peso")
+    tiempo2.value = localStorage.getItem("tiempo")
+}
+
+function sesion(){
+    altura = document.getElementById("alt").value;
+    peso = document.getElementById("peso").value;
+    tiempo = document.getElementById("tiempo").value;
+
+    sessionStorage.setItem("altura",altura);
+    sessionStorage.setItem("peso",peso);
+    sessionStorage.setItem("tiempo",tiempo);
+
+    localStorage.setItem("altura",altura);
+    localStorage.setItem("peso",peso);
+    localStorage.setItem("tiempo",tiempo);
+}
+
+
+function imprimir(cadena){
+    console.log(cadena)
+}
+
